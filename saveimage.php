6a404bc88da288e0e8e74a3057e324553047a2a3
Chris Carr <chris.carr@cobersolutions.com>
2017-03-04 18:22:11 -0500

    save bits

---
+++ saveimage.php
@@ -0,0 +1,13 @@
+<?php
+$img = $_REQUEST['data'];
+$img = str_replace('data:image/png;base64,', '', $img);
+$img = str_replace(' ', '+', $img);
+$id = uniqid();
+$user_agent = getenv("HTTP_USER_AGENT");
+if(strpos($user_agent, "Mac") !== FALSE){
+	$path = "/Applications/MAMP/htdocs/brainpower/img/";
+} else {
+	$path = "/var/www/html/";
+}
+file_put_contents($path. $id .'.png', base64_decode($img));
+?>
