b9d5462abc15def5a9ca27b89664a0c28b2b46dd
Chris Carr <chris.carr@cobersolutions.com>
2017-03-05 12:57:12 -0500

    load undone

--- loadscheduledevents.php
+++ loadscheduledevents.php
@@ -9,7 +9,7 @@ if(strpos($server_agent, "Darwin") !== FALSE){
 }
 $conn = mysqli_connect($servername, $username, $password, "mysql");
 
-$sql = "SELECT listsevent.id as listeventid, lists.id as id, name, description, image, video, audio, date, time, done, help FROM listsevent join lists on listid = lists.id order by time, date";
+$sql = "SELECT listsevent.id as listeventid, lists.id as id, name, description, image, video, audio, date, time, done, help FROM listsevent join lists on listid = lists.id order by time, date where done is null";
 $resultQuery = mysqli_query($conn, $sql);
 $results = [];
 while ($row = $resultQuery->fetch_object()){
