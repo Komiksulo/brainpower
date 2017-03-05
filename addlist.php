24066038820cc1057fa71aa53d45a3fadfdcda81
Chris Carr <chris.carr@cobersolutions.com>
2017-03-05 09:12:19 -0500

    build list

--- addlist.php
+++ addlist.php
@@ -1,16 +1,38 @@
 <?php
-
 	$servername = "localhost";
 	$username = "root";
 	$server_agent = php_uname();
 	if(strpos($server_agent, "Darwin") !== FALSE){
 		$password = "root";
+		$path = "/Applications/MAMP/htdocs/brainpower/img/";
 	} else {
 		$password = "f3b3cd0fd85768c63c0ec83bd75429202701e9703a002da0";
+		$path = "/var/www/html/img/";
 	}
 	$conn = mysqli_connect($servername, $username, $password, "mysql");
-	$sql = "INSERT INTO lists (name, description, image, audio, video, parent) values('".$_GET['name']."', '".$_GET['description']."', '".$_GET['image']."', '".$_GET['audio']."', '".$_GET['video']."', ".$_GET['parent'].")";
+	$image = $_POST['hiddenimage'];
+	if(isset($_FILES)){
+		if(isset($_FILES['image']) && $_files['image']['name'] != ""){
+			$file = $_FILES['image'];
+			rename($file['tmp_name'], $path.$file['name']);
+			$image = $file['name'];
+		}
+		$video = "";
+		if(isset($_FILES['video'])){
+			$file = $_FILES['video'];
+			rename($file['tmp_name'], $path.$file['name']);
+			$video = $file['name'];
+		}
+		$audio = "";
+		if(isset($_FILES['audio'])){
+			$file = $_FILES['audio'];
+			rename($file['tmp_name'], $path.$file['name']);
+			$audio = $file['name'];
+		}
+	}
+
+	$sql = "INSERT INTO lists (name, description, image, audio, video, parent) values('".$_POST['name']."', '".$_POST['description']."', '".$image."', '".$audio."', '".$video."', ".$_POST['parent'].")";
 	mysqli_query($conn, $sql);
 	mysqli_close($conn);
-
+	header('Location: '."buildlist.php");
 ?>
--- buildlist.php
+++ buildlist.php
@@ -4,8 +4,15 @@
 <script src="js/jquery-3.1.1.min.js"></script>
 </head>
 <body>
+<form action="addlist.php" method="post" enctype="multipart/form-data">
+	Add Task
 <div id="list0">
-</list>
+</div>
+<div><input type="submit" value="Save"> <input type="button" value="Cancel" onclick="window.location.href='caregiver.html';"></div>
+</form>
+<div>Utilities</div>
+<div><a href="recordaudio.html" target="_blank">Record Audio</a></div>
+<div><a href="recordvideo.html" target="_blank">Record Video</a></div>
 <script>
 function guid() {
   function s4() {
@@ -21,7 +28,7 @@ var imageId = guid();
 var counter = 0;
 var image;
 function addToList(id){
-	$("#"+id).append("Name <input type='text' id='name'> Description <textarea id='description'></textarea> Image <span class='imagefile'><input type='file'> or</span> <div id='imageplaceholder'><a href='recordimage.php?id="+imageId+"' target='_blank' onclick='isImageDone()'>Take a Picture</a></div>");
+	$("#"+id).append("<div>Title</div><div><input type='text' id='name' name='name'></div><div>Description</div><div><textarea name='description' id='description'></textarea></div> <div>Use Camera <input type='button' value='Take Picture' onclick='window.open(\"recordimage.php?id="+imageId+"\", \"_blank\"); isImageDone();'> <span id='imageplaceholder'>No picture</span></div> Upload Image <input type='file' name='image'> </div><input type='hidden' id='hiddenimage' name='hiddenimage'><input type='hidden' name='parent' value='-1'> <div>Upload Video <input type='file' name='video'></div> <div>Upload Audio <input type='file' name='audio'></div>");
 	counter += 1;
 }
 addToList("list0");
@@ -32,16 +39,12 @@ function isImageDone(){
 		if(result == false){
 			setTimeout(function(){isImageDone();}, 2500);
 		} else {
-			$("#imageplaceholder").html("<img src='img/"+imageId+".png' alt='capture'>");
-			$(".imagefile").hide();
-			image = imageId+".png";
+			$("#imageplaceholder").html(imageId+".png");
+			
+			$("#hiddenimage").val(imageId+".png");
 		}
 	});
 }
-function save(){
-	$.get("addlist.php?name="+$("#name").val()+"&description="+$("#description").val()+"&image="+image+"&video=&audio=&parent=-1");
-}
 </script>
-<div><input type="button" value="Save" onclick="save()"> <input type="button" value="Canacel"></div>
 </body>
 </html>
