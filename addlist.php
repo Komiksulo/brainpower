8fa784757c445213a84779bdd46d0952c423c1b0
Chris Carr <chris.carr@cobersolutions.com>
2017-03-04 21:11:28 -0500

    add to database

--- addlist.php
+++ addlist.php
@@ -2,13 +2,14 @@
 
 	$servername = "localhost";
 	$username = "root";
+	$server_agent = php_uname();
 	if(strpos($server_agent, "Darwin") !== FALSE){
-		$password = "F3ckth1s";
+		$password = "root";
 	} else {
 		$password = "f3b3cd0fd85768c63c0ec83bd75429202701e9703a002da0";
 	}
-	$conn = mysqli_connect($servername, $username, $password);
-	$sql = "INSERT INTO lists (name, description, image, audio, video, parent) values($_GET['name'], $_GET['description'], $_GET['image'], $_GET['audio'], $_GET['video'], $_GET['parent'])";
+	$conn = mysqli_connect($servername, $username, $password, "mysql");
+	$sql = "INSERT INTO lists (name, description, image, audio, video, parent) values('".$_GET['name']."', '".$_GET['description']."', '".$_GET['image']."', '".$_GET['audio']."', '".$_GET['video']."', ".$_GET['parent'].")";
 	mysqli_query($conn, $sql);
 	mysqli_close($conn);
 
