87e77ab15067d46b4966aca1a94f5c438ba725ac
Chris Carr <chris.carr@cobersolutions.com>
2017-03-04 20:03:33 -0500

    Insert into list

--- addlist.php
+++ addlist.php
@@ -1,5 +1,5 @@
 <?php
-function addList(){
+
 	$servername = "localhost";
 	$username = "root";
 	$password = "f3b3cd0fd85768c63c0ec83bd75429202701e9703a002da0";
@@ -7,5 +7,5 @@ function addList(){
 	$sql = "INSERT INTO lists (name, description, image, audio, video, parent) values($_GET['name'], $_GET['description'], $_GET['image'], $_GET['audio'], $_GET['video'], $_GET['parent'])";
 	mysqli_query($conn, $sql);
 	mysqli_close($conn);
-}
+
 ?>
